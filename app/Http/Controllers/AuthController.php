da4616720b7eb3347595814f84d6d5c3417c7d46
Jon Batista <batista.jonathas@gmail.com>
2021-07-09 18:36:57 -0300

    jwt configurado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,23 +6,36 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 'login';
+        $credenciais = $request->all(['email', 'password']);
+
+        $token = auth('api')->attempt($credenciais);
+
+        if ($token) { //usuario autenticado
+            return response()->json(['token' => $token], 200);
+        } else { //erro usuario ou senha
+            return response()->json([
+                'erro' => 'Usuario ou senha invalidos'
+            ], 403);
+        }
     }
 
     public function logout() 
     {
-        return 'logout';
+        auth('api')->logout();
+        return response()->json(['msg' => 'Logout foi realizado com sucesso']);
     }
 
     public function refresh() 
     {
-        return 'refresh';
+        //encaminhar jwt valido
+        $token = auth('api')->refresh();
+        return response()->json(['toke' => $token]);
     }
 
     public function me() 
     {
-        return 'me';
+        return response()->json(auth()->user());
     }
 }
